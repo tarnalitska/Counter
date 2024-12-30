e4e6faa53730ec3c8e1f70307a27e555a1dd67aa
Sofya Tarnalitskaya <tarnalitska@gmail.com>
2024-12-30 16:31:09 +0100

    Initial Commit

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Sofya Tarnalitskaya on 30/12/2024.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
