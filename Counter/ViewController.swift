062aa46cade23b02b5a92e91518c037fc3702f3b
Sofya Tarnalitskaya <tarnalitska@gmail.com>
2024-12-30 18:17:15 +0100

    App created

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var minusButton: UIButton!
+  @IBOutlet weak var plusButton: UIButton!
+  @IBOutlet weak var restartButton: UIButton!
+  
+  @IBOutlet weak var textView: UITextView!
+  @IBOutlet weak var counterLabel: UILabel!
+  var counter: Int = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    updateCounterLabel()
+    textView.isEditable = false
   }
 
-
+  // Buttons Actions (plus, minus, restart)
+  @IBAction func countMinus(_ sender: Any) {
+    let formattedDate = defineCurrentTime()
+    if counter > 0 {
+      counter -= 1
+      updateCounterLabel()
+      textView.text += "\n\(formattedDate) - значение изменено на -1"
+    } else {
+      textView.text += "\n\(formattedDate) - попытка уменьшить значение счётчика ниже 0"
+    }
+  }
+  
+  @IBAction func countPlus(_ sender: Any) {
+    let formattedDate = defineCurrentTime()
+    counter += 1
+    updateCounterLabel()
+    textView.text += "\n\(formattedDate) - значение изменено на +1"
+  }
+  
+  @IBAction func countRestart(_ sender: Any) {
+    let formattedDate = defineCurrentTime()
+    counter = 0
+    updateCounterLabel()
+    textView.text += "\n\(formattedDate) - значение сброшено"
+  }
+  
+  // Update Counter Label
+  private func updateCounterLabel() {
+    counterLabel.text = "Значение счётчика - \(counter)"
+  }
+  
+  // Define current time
+  private func defineCurrentTime() -> String {
+    let currentDate = Date()
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "ru_RU")
+    formatter.dateFormat = "dd MMMM yyyy HH:mm:ss"
+    
+    return formatter.string(from: currentDate)
+  }
 }
 
